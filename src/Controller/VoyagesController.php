4c1ffcf87e03bb1349f426a7546b8bf0a6c2e5ee
Sphayx <92910042+Sphayx@users.noreply.github.com>
2022-09-24 13:19:53 +0200

    Phase  8 : ORM(affichage en colonne des informations provenant de la BDD)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -7,6 +7,7 @@
 
 namespace App\Controller;
 
+use App\Repository\VisiteRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,6 +19,22 @@ class VoyagesController extends AbstractController{
  * @return Response
  */
     public function index(): Response{
-        return $this->render("pages/voyages.html.twig");
+        $visites = $this->repository->findAll();
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+    /**
+     * 
+     * @var VisiteRepository
+     */
+    private $repository;
+
+    /**
+    * 
+    * @param VisiteRepository $repository
+    */
+    public function __construct(VisiteRepository $repository){
+        $this->repository = $repository;
     }
 }
