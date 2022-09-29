d4ec0eb54ffa49c060e7747b0bf6cc82877fd347
Sphayx <92910042+Sphayx@users.noreply.github.com>
2022-09-29 08:51:37 +0200

    Phase 22 : codage de la page d'accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,17 +3,34 @@
 
 namespace App\Controller;
 
+use App\Repository\VisiteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class AccueilController extends AbstractController{
- /**
- * @Route("/", name="accueil")
- * @return Response
- */
+    /**
+     *
+     * @var VisiteRepository
+     */
+    private $repository;
+
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    public function __construct(VisiteRepository $repository) {
+        $this->repository = $repository;
+    }
+    /**
+     * @Route("/", name="accueil")
+     * @return Response
+     */
     public function index(): Response{
-        return $this->render("pages/accueil.html.twig");
+        $visites = $this->repository->findAllLasted(2);
+        return $this->render("pages/accueil.html.twig", [
+            'visites' => $visites
+        ]);   
     }
 }
